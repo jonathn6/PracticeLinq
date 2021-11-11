4a20d1c9d41d877a2a15b4205dc3c597e623a974
jonathn6 <jonathn6@icloud.com>
2021-11-11 16:58:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PracticeLinq!");
+
+            var videoGameList = new string[] { "Call of Duty", "Mario Brothers", "Spiderman","Minecraft","Grand Theft Auto","Fortnite" };
+            var sortedList = videoGameList.OrderBy(x => x.Length);
+            
+            foreach (var title in sortedList)
+            {
+                Console.WriteLine($"{title.Length} - {title}");
+            }
+
+        }
+    }
+}
